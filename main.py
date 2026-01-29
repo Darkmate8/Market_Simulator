a55e44948b2e0af74e90e13795cd797cd9b555df
Manan-Modi-cs <modi8.manan@gmail.com>
2026-01-29 23:36:07 +0530

    Goal 6A: asymmetric bots with aggressiveness and regimes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 price = 100
 bots = [10, 10, 10, 10, 10, 10]
 reference_price = [97, 100, 103, 95, 101, 99]
+agressivness = [3, 7, 9, 9, 5, 4]
 
 price_history = []
 bot_action_log = []
@@ -26,6 +27,9 @@ for step in range(50):
             market_regime = "low liquidity"
         else:
             market_regime = "high liquidity"
+            
+    # Random number generator
+    comparision_number = random.randint(1, 10)
     
     # 1. Impact finder
     if market_regime == "high liquidity":
@@ -44,19 +48,27 @@ for step in range(50):
     elif price == reference_price[index_bot]:
         chosen_action = "nothing"
 
+    if comparision_number < agressivness[index_bot]:
+        decision = "yes"
+    elif comparision_number > agressivness[index_bot]:
+        decision = "do not" 
+    
     # 3. Apply action
-    if chosen_action == "sell":
-        if bots[index_bot] > 0:
-            bots[index_bot] -= 1
-            price -= impact
+    if decision == "yes":
+        if chosen_action == "sell" :
+            if bots[index_bot] > 0:
+                bots[index_bot] -= 1
+                price -= impact
 
-    elif chosen_action == "buy":
-        bots[index_bot] += 1
-        price += impact
+        elif chosen_action == "buy":
+            bots[index_bot] += 1
+            price += impact
 
-    elif chosen_action == "nothing":
+        elif chosen_action == "nothing":
+            pass
+    elif decision == "do not":
         pass
-
+    
     # 4. Record state
     price_history.append(price)
     bot_action_log.append(f"bot {index_bot} {chosen_action}")
@@ -65,12 +77,15 @@ for step in range(50):
     if len(price_history) >= 2:
         price_change = price_history[-1] - price_history[-2]
 
-        if chosen_action == "buy" or chosen_action == "sell":
-            abs(price_change) == impact
-        elif chosen_action == "nothing":
-            abs(price_change) == 0
-        else:
-            raise Exception("Price changed by the wrong amount")
+        if decision == "yes":
+            if chosen_action == "buy" or chosen_action == "sell":
+                abs(price_change) == impact
+            elif chosen_action == "nothing":
+                abs(price_change) == 0
+            else:
+                raise Exception("Price changed by the wrong amount")
+        elif decision == "do not" and abs(price_change) > 0:
+            raise Exception("Price changed when it shouldn't")
             
         if chosen_action == "nothing" and price_change != 0:
             raise Exception("Price changed even though nothing happened")
